69c40af9dd679ecf67fb0e4069fba3b63efe23a7
Dylan Heinrich <harly13@live.com>
2019-02-16 18:11:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var array = []
+  var array = [];
   for(var i = 0; i < instruments.length;i++){
-    array.push(musicians[i] + ' plays ' + instruments[i])
+    array.push(musicians[i] + ' plays ' + instruments[i]);
   }
-  return array
+  return array;
 }
 
 function johnLennonFacts(array) {
-  var i = 0
+  var i = 0;
   while(i < array.length) {
-    array[i] = array[i] + '!!!'
-    i++
+    array[i] = array[i] + '!!!';
+    i++;
   }
-  return array
+  return array;
 }
 
 function iLoveTheBeatles(num) {
-  var array = []
+  var array = [];
   do {
-    array.push("I love the Beatles!")
-    num ++
+    array.push("I love the Beatles!");
+    num ++;
   }
-  while(num < 15)
-  return array
+  while(num < 15);
+  return array;
 }
